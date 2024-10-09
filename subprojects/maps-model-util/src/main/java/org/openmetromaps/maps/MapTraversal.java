bab7b9e98be1836bebfc2ea005f599b71029d50d
Mihály Dobos-Kovács <3751182+as3810t@users.noreply.github.com>
2024-10-09 18:22:45 +0000

    Add MapTraversal

---
+++ subprojects/maps-model-util/src/main/java/org/openmetromaps/maps/MapTraversal.java
@@ -0,0 +1,11 @@
+package org.openmetromaps.maps;
+
+import org.openmetromaps.maps.model.*;
+
+public class MapTraversal {
+    private MapTraversal() {}
+
+    public static int calculateExpectedRevenue(ModelData data) {
+        throw new RuntimeException("Not implemented");
+    }
+}
